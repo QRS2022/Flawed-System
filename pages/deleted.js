c7d1965a4c5ef1f8d6a6d41b990cfab689858539
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-06 16:52:54 +0800

    feat: add delete page

---
+++ pages/deleted.js
@@ -0,0 +1,50 @@
+import { Box, Modal, Typography } from "@mui/material";
+import React from "react";
+import MainLayout from "../components/mainLayout";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80vw",
+  height: "70vh",
+  bgcolor: "white !important",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+function Deleted() {
+  return (
+    <MainLayout>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          overflowX: "hidden",
+        }}
+      >
+        <Modal open={true} aria-labelledby="modal-modal-title">
+          <Box sx={modalStyle}>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{ fontSize: "3em" }}
+            >
+              Deleted successfully!
+            </Typography>
+          </Box>
+        </Modal>
+      </Box>
+    </MainLayout>
+  );
+}
+
+export default Deleted;
